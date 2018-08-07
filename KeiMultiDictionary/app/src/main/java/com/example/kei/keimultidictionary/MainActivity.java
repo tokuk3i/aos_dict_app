dd9eac0d9248c43760bfeadccec6ea1709f18d7c
CWJP Kei <khanh@co-well.jp>
2018-08-07 16:19:15 +0900

    no message

--- KeiMultiDictionary/app/src/main/java/com/example/kei/keimultidictionary/MainActivity.java
+++ KeiMultiDictionary/app/src/main/java/com/example/kei/keimultidictionary/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.kei.keimultidictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     SearchView searchView;
     ListView listView;
+    TextView contentView;
     /*String[] wordList;
     ArrayAdapter<String> arrayAdapter;*/
     ListEntryAdapter entriesAdapter;
@@ -27,6 +32,9 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
         listView = (ListView)findViewById(R.id.listsugview);
 
+        contentView = (TextView) findViewById(R.id.contentview);
+        contentView.setVisibility(View.INVISIBLE);
+
 
         entriesList = new ArrayList<WordEntry>();
         WordEntry entry1 = new WordEntry("hello","xin chao",1);
@@ -38,6 +46,19 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         entriesAdapter.setWordList(entriesList);
 
         listView.setAdapter(entriesAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                WordEntry entry = (WordEntry) o;
+                listView.setVisibility(View.INVISIBLE);
+                contentView.setText(entry.getMean());
+                contentView.setVisibility(View.VISIBLE);
+                //Toast.makeText(MainActivity.this, "Selected :" + " " + country, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
