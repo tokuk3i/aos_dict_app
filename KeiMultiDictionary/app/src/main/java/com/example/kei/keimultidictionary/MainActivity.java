d281496f432913a936f804ba8cb93e69ef168549
CWJP Kei <khanh@co-well.jp>
2018-08-07 12:15:56 +0900

    no message

--- KeiMultiDictionary/app/src/main/java/com/example/kei/keimultidictionary/MainActivity.java
+++ KeiMultiDictionary/app/src/main/java/com/example/kei/keimultidictionary/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.kei.keimultidictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    SearchView searchView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = (SearchView)findViewById(R.id.searchview);
+        searchView.setOnQueryTextListener(this);
+
+        listView = (ListView)findViewById(R.id.listsugview);
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        /*String text = newText;
+        adapter.filter(text);
+        */
+        return false;
     }
 }
---
+++ KeiMultiDictionary/app/src/main/java/com/example/kei/keimultidictionary/WordEntry.java
@@ -0,0 +1,7 @@
+package com.example.kei.keimultidictionary;
+
+public class WordEntry {
+    private String word;
+    private String mean;
+    private Integer dict_id;
+}
